990f3f9fded682218b952e4c3ddb395099328596
YooChibu <dbclqn@gmail.com>
2026-01-29 20:20:56 +0900

    Updates

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = "chipblockcrush-v1";
+
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches
+      .keys()
+      .then((names) => {
+        return Promise.all(
+          names
+            .filter((name) => name !== CACHE_NAME)
+            .map((name) => caches.delete(name)),
+        );
+      })
+      .then(() => self.clients.claim()),
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request)),
+  );
+});
